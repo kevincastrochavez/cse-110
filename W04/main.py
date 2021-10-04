fa977055dfbbb2789f1ee8f6d38e67874b2e2148
Kevin Castro <kevin.castro.chavez0@gmail.com>
2021-10-04 17:50:51 -0600

    completed assignment preparation for week 4

---
+++ W04/main.py
@@ -0,0 +1,4 @@
+tempFarenheit = int(input('What is the temperature in Fahrenheit?: '))
+tempCelsius = (tempFarenheit - 32) / 1.8
+
+print(f'The temperature in Celsius is {tempCelsius:.1f} degrees.')
