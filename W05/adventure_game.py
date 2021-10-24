9a91f73b13dc953ab7235f7bfd48f77256c43448
Kevin Castro <kevin.castro.chavez0@gmail.com>
2021-10-23 19:11:49 -0600

    modified the adventure game programm to fulfill requirements for the assignment

--- W05/adventure_game.py
+++ W05/adventure_game.py
@@ -1,8 +1,8 @@
 print('')
 print('You are standing in a dark room.')
-print('There is a door to your left and right')
+print('There is a door to your LEFT and RIGHT')
 print('')
-first_scenario = input('Which one do you take? (left or right): ').lower().strip()
+first_scenario = input('Which one do you take? (LEFT or RIGHT): ').lower().strip()
 print('')
 
 if first_scenario == 'left':
@@ -10,12 +10,12 @@ if first_scenario == 'left':
     print("Behind the bear is another door.")
     print("The bear is eating tasty honey!")
     print('')
-    second_scenario = input("What would you do? (take the honey, taunt the bear or run): ").lower().strip()
+    second_scenario = input("What would you do? (TAKE THE HONEY, TAUNT THE BEAR or RUN): ").lower().strip()
     print('')
     if second_scenario == 'take the honey':
         print('The bear killed you.')
         print('')
-        third_scenario = input('Where would you like to go? (Heaven or hell): ').lower().strip()
+        third_scenario = input('Where would you like to go? (HEAVEN or HELL): ').lower().strip()
         print('')
         if third_scenario == 'heaven':
             print("Now you'll have free honey for eternity!")
@@ -27,7 +27,9 @@ if first_scenario == 'left':
             print("Don't you know how to type something properly?")
             print('')
     elif second_scenario == 'taunt the bear':
-        third_scenario = input('Your good time, the bear moved from the door. You can go through it now! What do you do now? (Go in or stay out): ').lower().strip()
+        print('Your good time, the bear moved from the door. You can go through it now!')
+        print('')
+        third_scenario = input('What do you do now? (GO IN or STAY OUT): ').lower().strip()
         print('')
         if third_scenario == 'go in':
             print('You passed you CSE 110 class!')
@@ -39,7 +41,9 @@ if first_scenario == 'left':
             print("Don't you know how to type something properly?")
             print('')
     elif second_scenario == 'run':
-        third_scenario = input('The bear is now following you and you are running away for your life. What do you do now? (Fight the bear or drop candies): ').lower().strip()
+        print('The bear is now following you and you are running away for your life.')
+        print('')
+        third_scenario = input('What do you do now? (FIGHT THE BEAR or DROP CANDIES): ').lower().strip()
         print('')
         if third_scenario == 'fight the bear':
             print('Poor decision. The bear killed you.')
@@ -55,14 +59,15 @@ if first_scenario == 'left':
 elif first_scenario == 'right':
     print("Now you entered the room of a monster!")
     print("The monster is sleeping.")
+    print('Behind the monster, there is another door.')
     print('')
-    second_scenario = input('Behind the monster, there is another door. What would you do? (Go slilently, kill him or feed him): ')
+    second_scenario = input('What would you do? (GO SILENTLY, KILL HIM or FEED HIM): ')
     print('')
     if second_scenario == 'go silently':
         print('Lucky you!')
         print("You are now in a room filled with diamonds!")
         print('')
-        third_scenario = input('What would you do? (Take some or leave them): ').lower().strip()
+        third_scenario = input('What would you do? (TAKE SOME or LEAVE THEM): ').lower().strip()
         print('')
         if third_scenario == 'take some':
             print('The monster killed you. You are a robber')
@@ -75,7 +80,7 @@ elif first_scenario == 'right':
     elif second_scenario == 'kill him':
         print('Good try but the monster killed you first')
         print('')
-        third_scenario = input('Where would you like to go? (Heaven or hell): ').lower().strip()
+        third_scenario = input('Where would you like to go? (HEAVEN or HELL): ').lower().strip()
         print('')
         if third_scenario == 'heaven':
             print("Now you'll have all the diamonds for eternity!")
@@ -90,7 +95,7 @@ elif first_scenario == 'right':
         print('The monster is very happy with you!')
         print('He had not eaten in a while')
         print('')
-        third_scenario = input('He offers you the diamonds. What do you do? (Take them or reject them): ').lower().strip()
+        third_scenario = input('He offers you the diamonds. What do you do? (TAKE THEM or REJECT THEM): ').lower().strip()
         print('')
         if third_scenario == 'take them':
             print('Everything is alright. You deserved them')
