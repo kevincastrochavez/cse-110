6a3fa99a24e07d694dc915af3f86e4bdc294dee1
Kevin Castro <kevin.castro.chavez0@gmail.com>
2021-10-25 10:44:47 -0600

    finished checkpoint activity for week 7

---
+++ W07/loops.py
@@ -0,0 +1,13 @@
+positive_number = int(input('Please type a positive number: '))
+
+while positive_number < 0:
+    positive_number = int(input('Sorry, that is a negative number. Please try again: '))
+
+print(f'The number is: {positive_number}')
+
+
+candy = input('May I have a piece of candy?: ')
+while candy.lower() == 'no':
+    candy = input('May I have a piece of candy?: ')
+
+print('Thank you')
