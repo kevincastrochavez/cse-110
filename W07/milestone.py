29de4099035b862c2705b8efbb64e344910844bc
Kevin Castro <kevin.castro.chavez0@gmail.com>
2021-10-30 14:45:14 -0600

    finished deliverable milestone for week 7

--- W07/milestone.py
+++ W07/milestone.py
@@ -5,9 +5,16 @@ image_original = Image.open('penguin.jpg')
 
 width, height = image_original.size
 pixels_original = image_original.load()
+
 r, g, b = pixels_original[100, 200]
+
 print(r)
 print(g)
 print(b)
 
+for i in range(0, 100):
+    for j in range(0, 100):
+        pixels_original[100 + i, 200 + j] = 255, 0, 255
+
 image_original.show()
+image_original.save("penguin_modified.jpg")
