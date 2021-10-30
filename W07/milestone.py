16b556bb049a2bb7396ad291c93e4c55d958b869
Kevin Castro <kevin.castro.chavez0@gmail.com>
2021-10-30 14:58:11 -0600

    created function to modify pixels passing the coordinates and rgb color

--- W07/milestone.py
+++ W07/milestone.py
@@ -1,4 +1,3 @@
-# This line imports or includes the library we will need
 from PIL import Image
 
 image_original = Image.open('penguin.jpg')
@@ -6,15 +5,17 @@ image_original = Image.open('penguin.jpg')
 width, height = image_original.size
 pixels_original = image_original.load()
 
-r, g, b = pixels_original[100, 200]
+def modify_pixels(x_axis, y_axis, red, green, blue):
+    for i in range(0, 10):
+        for j in range(0, 10):
+            pixels_original[x_axis + i, y_axis + j] = red, green, blue
 
-print(r)
-print(g)
-print(b)
-
-for i in range(0, 100):
-    for j in range(0, 100):
-        pixels_original[100 + i, 200 + j] = 255, 0, 255
+modify_pixels(100, 200, 255, 0, 255)
+modify_pixels(200, 200, 193, 45, 230)
+modify_pixels(300, 200, 7, 210, 183)
+modify_pixels(400, 200, 48, 194, 39)
+modify_pixels(500, 200, 149, 23, 1)
 
 image_original.show()
 image_original.save("penguin_modified.jpg")
+
