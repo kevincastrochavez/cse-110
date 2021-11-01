6033c8bbc0428a042c2e3b1814e37fc968cbf6fe
Kevin Castro <kevin.castro.chavez0@gmail.com>
2021-11-01 14:55:48 -0600

    finished checkpoint program for week 8

---
+++ W08/loops.py
@@ -0,0 +1,11 @@
+colors = ["red", "blue", "green", "yellow"]
+numbers = [1, 2, 3, 4, 5, 6, 7, 8]
+
+for color in colors:
+    print(color)
+
+for number in numbers:
+    print(number)
+
+for item in range(2, 21, 2):
+    print(item)
