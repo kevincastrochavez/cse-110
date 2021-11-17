bd67da0691907349bb279c165901cbb9abfe3594
Kevin Castro <kevin.castro.chavez0@gmail.com>
2021-11-17 11:14:15 -0700

    finished stretch challenges for the team activity on week 10

--- W10/accounts.py
+++ W10/accounts.py
@@ -18,10 +18,30 @@ while account != 'quit':
 print('')
 print('Account Information:')
 for item in range(0, len(bank_accounts)):
-    print(f'{bank_accounts[item]} - ${balances[item]}')
+    print(f'{item}. {bank_accounts[item]} - ${balances[item]}')
 
 total = reduce(lambda a,b: a + b, balances)
 average = total / len(balances)
+highest_balance = max(balances)
+
 print('')
 print(f'Total: ${total}')
 print(f'Average" ${average:.2f}')
+print(f'Highest balance: savings - ${highest_balance}')
+
+update_account = 'yes'
+
+while update_account == 'yes':
+    print('')
+    update_account = input('Do you want to update an account? ')
+    
+    if update_account == 'yes':
+        account_to_update = int(input('What account index do you want to update? '))
+        new_amount = input('What is the new amount? ')
+
+        balances[account_to_update] = new_amount
+
+    print('')
+    print('Account Information:')
+    for item in range(0, len(bank_accounts)):
+        print(f'{item}. {bank_accounts[item]} - ${balances[item]}')
