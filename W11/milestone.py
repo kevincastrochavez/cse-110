7c8947705c9ddc4a259629cf8f96ee69abcc8748
Kevin Castro <kevin.castro.chavez0@gmail.com>
2021-11-24 20:28:03 -0700

    finished team activity for week 11 and started the milestone

--- W11/milestone.py
+++ W11/milestone.py
@@ -1,12 +1,23 @@
 with open("life-expectancy.csv") as life_file:
     next(life_file)
-    
-    life_list = []
+
+    # life_list = []
+    # countries = []
+    # codes = []
+    years = []
+    indexes = []
     
     for line in life_file:
-        life_list.append(line.split(',')[3])
+        # countries.append(line.split(',')[0])
+        # codes.append(line.split(',')[1])
+        years.append(line.split(',')[2])
+        # life_list.append(line.split(',')[3])
 
-    print(max(life_list))
-    print(min(life_list))
 
+    # print(years)
+    # print(index)
+    # print(years[index])
 
+# index = years.index('2019')
+#         print(years[index])
+#         # indexes.append(index)
---
+++ W11/team_activity.py
@@ -0,0 +1,17 @@
+with open("hr_system.txt") as f:
+    for line in f:
+        clean_line = line.strip()
+
+        parts = clean_line.split(" ")
+
+        name = parts[0]
+        id_number = parts[1]
+        title = parts[2]
+        salary = float(parts[3])
+
+        pay_amount = salary / 24
+
+        if title.lower() == "engineer":
+            pay_amount += 1000
+
+        print(f"{name} (ID: {id_number}), {title} - ${pay_amount:.2f}")
