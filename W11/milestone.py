9854dead1f0ddd33ca35f4f125ba8a951a746035
Kevin Castro <kevin.castro.chavez0@gmail.com>
2021-11-26 13:17:48 -0700

    finished all basic requirements for proven assignment in week 12

--- W11/milestone.py
+++ W11/milestone.py
@@ -2,9 +2,10 @@ with open("life-expectancy.csv") as life_file:
     next(life_file)
 
     data_set = []
+    year_user = int(input('Enter the year of interest: '))
+
     life_expectancy_sum = 0
     life_expectancy_length = 0
-    year_user = int(input('Enter the year of interest: '))
 
     max_overall_life = 0
     max_overall_country = ""
@@ -12,6 +13,11 @@ with open("life-expectancy.csv") as life_file:
     min_overall_life = 150
     min_overall_country = ""
     min_overall_year = 3000
+
+    max_overall_life_per_year = 0
+    max_overall_country_per_year = ""
+    min_overall_life_per_year = 150
+    min_overall_country_per_year = ""
     
     for line in life_file:
         clean_line = line.strip()
@@ -35,7 +41,14 @@ with open("life-expectancy.csv") as life_file:
         if year == year_user:
             life_expectancy_length += 1
             life_expectancy_sum += years_expected
-    
+
+            if years_expected > max_overall_life_per_year:
+                max_overall_life_per_year = years_expected
+                max_overall_country_per_year = country
+
+            if years_expected < min_overall_life_per_year:
+                min_overall_life_per_year = years_expected
+                min_overall_country_per_year = country
 
     average_life = life_expectancy_sum / life_expectancy_length
 
@@ -46,3 +59,6 @@ with open("life-expectancy.csv") as life_file:
     print('')
     print(f'For the year {year_user}')
     print(f'The average life expectancy across all countries was {average_life:.2f}')
+    print(f'The max life expectancy was in {max_overall_country_per_year} with {max_overall_life_per_year}')
+    print(f'The min life expectancy was in {min_overall_country_per_year} with {min_overall_life_per_year}')
+    
