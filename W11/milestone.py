2ae97011695c15b2c5635e600d8e55334e4256e2
Kevin Castro <kevin.castro.chavez0@gmail.com>
2021-11-25 22:06:08 -0700

    finished code to get average life expactany across all countries

--- W11/milestone.py
+++ W11/milestone.py
@@ -1,13 +1,19 @@
+from functools import reduce
+
 with open("life-expectancy.csv") as life_file:
     next(life_file)
 
     data_set = []
+    life_expectancy_sum = 0
+    life_expectancy_length = 0
     year_user = int(input('Enter the year of interest: '))
     print('')
     
     for line in life_file:
         clean_line = line.strip()
         data_set.append(clean_line.split(','))
+        life_expectancy_sum += float(clean_line.split(',')[3])
+        life_expectancy_length += 1
 
     max_overall_life = 0
     max_overall_country = ""
@@ -15,6 +21,8 @@ with open("life-expectancy.csv") as life_file:
     min_overall_life = 150
     min_overall_country = ""
     min_overall_year = 3000
+
+    average_life = life_expectancy_sum / life_expectancy_length
 
     for item in data_set:
         country = item[0] 
@@ -33,23 +41,7 @@ with open("life-expectancy.csv") as life_file:
 
     print(f'The overall max life expectancy is: {max_overall_life} from {max_overall_country} in {max_overall_year}')
     print(f'The overall min life expectancy is: {min_overall_life} from {min_overall_country} in {min_overall_year}')
-    
-    # for list in data_set:
-    #     life_time = list[3]
-    #     country = list[0]
 
-    #     if int(list[2]) == year:
-
-# max_price = -1
-# max_product = "" # It doesn't matter what you set this to, it just needs to be declared
-
-# for item in shopping_cart:
-#     product_name = item[0] # Product name is the first part
-#     price = item[1] 
-
-#     if price > max_price:
-#         # This is the new max
-#         max_price = price
-
-#         # Also save this product name as the max product
-#         max_product = product_name
+    print('')
+    print(f'For the year {year_user}')
+    print(f'The average life expectancy across all countries was {average_life:.2f}')
