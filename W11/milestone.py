20f514af1dee7a230035c4d9591a4ebed7b23974
Kevin Castro <kevin.castro.chavez0@gmail.com>
2021-11-25 20:02:38 -0700

    polished milestone activity

--- W11/milestone.py
+++ W11/milestone.py
@@ -1,23 +1,11 @@
 with open("life-expectancy.csv") as life_file:
     next(life_file)
 
-    # life_list = []
-    # countries = []
-    # codes = []
-    years = []
-    indexes = []
+    life_expectancy = []
     
     for line in life_file:
-        # countries.append(line.split(',')[0])
-        # codes.append(line.split(',')[1])
-        years.append(line.split(',')[2])
-        # life_list.append(line.split(',')[3])
+        life_expectancy.append(line.split(',')[3])
 
 
-    # print(years)
-    # print(index)
-    # print(years[index])
-
-# index = years.index('2019')
-#         print(years[index])
-#         # indexes.append(index)
+    print(f'Highest life expectancy: {max(life_expectancy)}')
+    print(f'Lowest life expectancy: {min(life_expectancy)}')
